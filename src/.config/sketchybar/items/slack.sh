5fd2a565c67f327e780fff3c8659df337f6323b2
Shelton Tolbert <tolbert.shelton@gmail.com>
2025-05-02 08:38:31 -0700

    Init sketchybar config

---
+++ src/.config/sketchybar/items/slack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+slack=(
+    updates=on
+    update_freq=3
+    icon.drawing=off
+    label.font="$FONT:Semibold:14.0"
+    script="$PLUGIN_DIR/slack.sh"
+)
+
+sketchybar --add item slack left        \
+           --set slack "${slack[@]}"  \
